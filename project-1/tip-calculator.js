f7c15236d51aa9ffcfd3516fd9ce1bd46e14b7e7
Michael <mickeymakesapps88@gmail.com>
2021-05-11 19:33:10 -0700

    completed project

--- project-1/tip-calculator.js
+++ project-1/tip-calculator.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+const button = document.querySelector("button");
+const output = document.querySelector(".output");
+const cost   = document.querySelector("input");
+console.log(button);
+button.addEventListener("click", function(){
+    console.log(cost.value *0.15);
+    let tip    = (cost.value*0.15).toFixed(2);
+    let templt = `<h2>You should tip &dollar;${tip} on &dollar;${cost.value}</h2>`;
+    output.innerHTML = templt;
+});
