21b2a3d9a5f533e126813a3c92c4bc5d3aee833f
Michael <mickeymakesapps88@gmail.com>
2021-05-12 12:00:00 -0700

    completed project

--- project-2/custom-message.js
+++ project-2/custom-message.js
@@ -1,1 +1,10 @@
-console.log("Hello World, from custom-message.js!");
+const output = document.querySelector(".output");
+const button = document.querySelector("button");
+const myName = document.querySelector("input");
+console.log(button);
+
+button.addEventListener("click", showMessage);
+
+function showMessage(){
+    output.innerHTML = `<h2>Hello and welcome, ${myName.value}!</h2>`;
+}
