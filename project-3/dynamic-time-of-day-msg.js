a90fc3397f96fc1ee4656fc820a32a8e8552dc17
Michael <mickeymakesapps88@gmail.com>
2021-05-13 08:50:57 -0700

    completed project

--- project-3/dynamic-time-of-day-msg.js
+++ project-3/dynamic-time-of-day-msg.js
@@ -1,1 +1,26 @@
-console.log("Hello from \"dynamic-time-of-day-msg.js!\"");
+const output = document.querySelector(".output");
+const button = document.querySelector("button");
+
+button.addEventListener("click", showOutput);
+
+function showOutput(){
+    const date  = new Date();
+    let current = date.getHours();
+    let message;
+    
+    if(current > 16){
+        message = "It's evening";
+        output.style.backgroundColor = "black";
+    } else if(current >= 12){
+        message = "It's afternoon";
+        output.style.backgroundColor = "orange";
+    } else if(current >= 0){
+        message = "It's morning";
+        output.style.backgroundColor = "blue";
+    } else {
+        message = "Oops! Something went wrong... Please try again.";
+        output.style.backgroundColor = "red";
+    }
+
+    output.innerHTML = `<h1>${message}</h1>`;
+}
