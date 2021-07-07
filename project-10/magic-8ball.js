b5902bf7b81fcf6f6dda6fb2464eff4554d00e42
Michael <mickeymakesapps88@gmail.com>
2021-07-07 14:24:48 -0700

    completed project

--- project-10/magic-8ball.js
+++ project-10/magic-8ball.js
@@ -1,1 +1,12 @@
-console.log("Hello from 'magic-8ball.js'!");
+const answerArray = ["It will work","Maybe, maybe not","Probably not","Highly likely","I don't know"]
+const message  = document.querySelector(".message");
+const question = document.querySelector("input");
+const button   = document.querySelector("button");
+
+button.addEventListener("click", function(){
+    // console.log(question.value);
+    let res = Math.floor(Math.random()*answerArray.length);
+    // console.log(answerArray[res]);
+    message.innerHTML = `<strong>${answerArray[res]}.</strong>`;
+
+});
