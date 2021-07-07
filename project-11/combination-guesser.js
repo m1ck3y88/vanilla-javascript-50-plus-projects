369c9fe4b1159d49d037942ac5d4a3e2b1c32ad5
Michael <mickeymakesapps88@gmail.com>
2021-07-07 15:53:33 -0700

    removed duplicate project directory and added style.css file

--- project-11/combination-guesser.js
+++ project-11/combination-guesser.js
@@ -1,1 +1,30 @@
-console.log("Hello from 'combination-guesser.js'!");
+const gameArea = document.querySelector(".game");
+const button = document.querySelector("button");
+let gamePlay = false;
+button.addEventListener("click", function(){
+    if(!gamePlay){
+        gamePlay = true;
+        maker();
+        button.innerText = "Check Combo";
+    }else{
+        console.log("checker");
+    }
+    
+});
+
+function maker(){
+    for(let i = 0; i < 4; i++){
+    let el = document.createElement("input");
+    el.setAttribute("type","number");
+    el.max = 9;
+    el.min = 0;
+    el.size = 1;
+    el.style.width = "50px";
+    el.classList.add("numb");
+    el.value = 0;
+    el.order = i;
+    console.log(el);
+    gameArea.appendChild(el);
+    }
+    
+}
